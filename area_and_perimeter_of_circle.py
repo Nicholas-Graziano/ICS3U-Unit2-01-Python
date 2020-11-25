b879f47e1fe1c62fde15abfba9bf01c31496b2c4
Ubuntu <ubuntu@ip-172-31-9-20.ec2.internal>
2020-11-25 17:30:37 +0000

    added area_ and_ perimeter_ of_ a_ cricle.py

---
+++ area_and_perimeter_of_circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Nicholas Graziano
+# Created on: November 2020
+# This program calculates the area and perimeter of a circle
+#       with radius 15mm
+
+import math
+
+
+def main():
+    print("If circle has a radius of 15mm:")
+    print("")
+    print("Area is {}mm^2".format(math.pi*15**2))
+    print("perimeter is {}mm".format(2*math.pi*15))
+
+
+if __name__ == "__main__":
+    main()
